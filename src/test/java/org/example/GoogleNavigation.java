536e374497eb7d00ec50b9fcb4fea3cbc50348d5
chathurahjm <chathudagr8@yahoo.com>
2023-12-13 15:45:55 +0530

    Update GoogleNavigation.java

--- src/test/java/org/example/GoogleNavigation.java
+++ src/test/java/org/example/GoogleNavigation.java
@@ -5,7 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GoogleNavigation {
-    public static void main(String[] args) {
+    @Test
+    public void testActions() {
         // Setup WebDriverManager for Chrome
         WebDriverManager.chromedriver().setup();
 
