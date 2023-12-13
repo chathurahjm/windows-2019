b8e94415b2e8f1196221396f4779d33d9c0cb768
chathurahjm <chathudagr8@yahoo.com>
2023-12-13 15:49:59 +0530

    Add files via upload

---
+++ src/test/java/org/example/GoogleNavigation.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class GoogleNavigation {
+    @Test
+    public void testActions() {
+        // Setup WebDriverManager for Chrome
+        WebDriverManager.chromedriver().setup();
+
+        // Create a WebDriver instance (ChromeDriver)
+        WebDriver driver = new ChromeDriver();
+
+        // Navigate to Google
+        driver.get("https://www.google.com");
+
+        // Close the browser
+        driver.quit();
+    }
+}
