1897f61a64985aa47b5354338f5ede0da78c25fe
chathurahjm <chathudagr8@yahoo.com>
2023-12-13 15:57:21 +0530

    Update GoogleNavigation.java

--- src/test/java/org/example/GoogleNavigation.java
+++ src/test/java/org/example/GoogleNavigation.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-
+@Test
 public class GoogleNavigation {
     @Test
     public void testActions() {
