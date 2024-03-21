c7e2872ec3c0f5f556bfd7f6214cabb7ce671822
chathurahjm <chathudagr8@yahoo.com>
2024-03-21 19:37:22 +0530

    Update GoogleNavigation.java

--- src/test/java/org/example/GoogleNavigation.java
+++ src/test/java/org/example/GoogleNavigation.java
@@ -47,7 +47,7 @@ public class GoogleNavigation {
         //WebDriverManager.chromedriver().setup();
 
          ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless");
+        //chromeOptions.addArguments("--headless");
 
         // Set headless mode
         chromeOptions.addArguments("--disable-web-security");
