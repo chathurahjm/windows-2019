ee039155c99f7c63c71922af6be041c9edee46dd
chathurahjm <chathudagr8@yahoo.com>
2024-03-26 08:43:53 +0530

    Update GoogleNavigation.java

--- src/test/java/org/example/GoogleNavigation.java
+++ src/test/java/org/example/GoogleNavigation.java
@@ -1,6 +1,4 @@
 package org.example;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
