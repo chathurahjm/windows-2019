b55f429da5472beaa366d6c3b5c396b64ea83ce4
chath <we@as.sd>
2024-03-26 11:46:44 +0530

    fdvgevf

--- src/test/java/org/example/vpntest.java
+++ src/test/java/org/example/vpntest.java
@@ -31,6 +31,9 @@ public class vpntest {
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 
         ChromeOptions chromeOptions = new ChromeOptions();
+             chromeOptions.addArguments("--disable-extensions");
+             chromeOptions.addArguments("--disable-gpu");
+             chromeOptions.addArguments("--no-sandbox");
         chromeOptions.addArguments("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36");
 
         //chromeOptions.addArguments("--headless");
